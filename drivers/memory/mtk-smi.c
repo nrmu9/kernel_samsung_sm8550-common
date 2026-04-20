a406adb3bf48ddd9a91eaa4d28c2b4f4d8e28a57
Greg Kroah-Hartman <gregkh@google.com>
2026-04-20 12:21:31 +0000

    Revert "memory: mtk-smi: Convert to platform remove callback returning void"
    This reverts commit df89a4ea2c01af0f3a3a7b0082990bcd39f12a1c which is
    commit 08c1aeaa45ce0fd18912e92c6705586c8aa5240f upstream.
    
    It breaks the Android kernel abi and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I5ea4880a310146bbf3868afcdb41e99962f7f19c
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/memory/mtk-smi.c
+++ drivers/memory/mtk-smi.c
@@ -375,13 +375,14 @@ static int mtk_smi_larb_probe(struct platform_device *pdev)
 	return component_add(dev, &mtk_smi_larb_component_ops);
 }
 
-static void mtk_smi_larb_remove(struct platform_device *pdev)
+static int mtk_smi_larb_remove(struct platform_device *pdev)
 {
 	struct mtk_smi_larb *larb = platform_get_drvdata(pdev);
 
 	device_link_remove(&pdev->dev, larb->smi_common_dev);
 	pm_runtime_disable(&pdev->dev);
 	component_del(&pdev->dev, &mtk_smi_larb_component_ops);
+	return 0;
 }
 
 static int __maybe_unused mtk_smi_larb_resume(struct device *dev)
@@ -418,7 +419,7 @@ static const struct dev_pm_ops smi_larb_pm_ops = {
 
 static struct platform_driver mtk_smi_larb_driver = {
 	.probe	= mtk_smi_larb_probe,
-	.remove_new = mtk_smi_larb_remove,
+	.remove	= mtk_smi_larb_remove,
 	.driver	= {
 		.name = "mtk-smi-larb",
 		.of_match_table = mtk_smi_larb_of_ids,
@@ -548,9 +549,10 @@ static int mtk_smi_common_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static void mtk_smi_common_remove(struct platform_device *pdev)
+static int mtk_smi_common_remove(struct platform_device *pdev)
 {
 	pm_runtime_disable(&pdev->dev);
+	return 0;
 }
 
 static int __maybe_unused mtk_smi_common_resume(struct device *dev)
@@ -586,7 +588,7 @@ static const struct dev_pm_ops smi_common_pm_ops = {
 
 static struct platform_driver mtk_smi_common_driver = {
 	.probe	= mtk_smi_common_probe,
-	.remove_new = mtk_smi_common_remove,
+	.remove = mtk_smi_common_remove,
 	.driver	= {
 		.name = "mtk-smi-common",
 		.of_match_table = mtk_smi_common_of_ids,
